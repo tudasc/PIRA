66f50956f569db3568b1a8057342ac94cac6234f
Jan Lehr <jp.lehr@gmail.com>
2017-01-09 22:34:56 +0100

    Fixed typo

--- functors/spec_vanilla_run_gen.py
+++ functors/spec_vanilla_run_gen.py
@@ -8,7 +8,7 @@ def generate(benchmark, **kwargs):
 
     args = ba.benchmark_args[benchmark]
     sub_email = 'submit.notifications@gmail.com'
-    out_file_name = 'generated_run_' + benchmark.replace(' ', '_') + '_finstr.sh'
+    out_file_name = 'generated_run_' + benchmark.replace(' ', '_') + '_vanilla.sh'
     util.shell('sed -e "s;SUBMAIL;' + sub_email + ';g" -e "s;BENCHMARK;' + benchmark
                + ';g" -e "s;FLAVOR;vanilla;g" -e "s;INVOK;' + args + ';g" '
                + ba.template_file + ' > ' + out_file_name, dry=True)
