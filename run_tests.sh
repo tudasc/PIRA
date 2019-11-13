d7027dc09d2c028246991434e6fe8518aecbd918
Jan Lehr <jp.lehr@gmail.com>
2019-11-13 12:42:49 +0100

    Gitlab CI should now start failing unit tests step
    - Gitlab CI now runs the available unit tests for PIRA, which currently fail
    - The exit code tells the number of different unit test components that fail
    
    (asana) #1146121975356559

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,7 @@
 echo "Running the PIRA tests"
+didfail=0
 cd test
-for i in `find . -iname "*.py"`; do echo "Running $i"; python3 $i; done
+for i in `find . -iname "*.py"`; do echo "Running $i"; python3 $i; if [ $? -ne 0 ]; then let didfail=$didfail+1; fi ; done
 cd ..
+
+exit $didfail
