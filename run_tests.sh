0b73559508cb57539154b55ac7875968ff0f5fed
Jan Lehr <jp.lehr@gmail.com>
2020-01-30 00:21:58 +0100

    Removes out of date files
    (asana) #1159396822971041

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,25 @@
 echo "Running the PIRA tests"
 
+tester=''
+
+command -v pytest
+if [ $? -eq 1 ]; then
+  echo -e "[PIRA]: No pytest found.\n"
+else
+  tester=pytest
+fi
+
+command -v pytest-3
+if [ $? -eq 1 ]; then
+  echo -e "[PIRA]: No pytest-3 found.\n"
+else
+  tester=pytest-3
+fi
+
+if [ -z $tester ]; then
+  exit -1
+fi
+
 didfail=0
 
 export PYTHONPATH=$PWD:$PYTHONPATH
@@ -9,7 +29,7 @@ export PATH=~/.local/bin:$PATH
 cd test/unit
 for i in `find . -iname "*.py"`; do 
 	echo "Running $i"; 
-  pytest --cov=lib --cov-append $i; 
+  $tester --cov=lib --cov-append $i; 
 	if [ $? -ne 0 ]; then 
 		let didfail=$didfail+1; 
 	fi ; 
